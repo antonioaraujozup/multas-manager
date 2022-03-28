1727994794792abea71aa5fe0e486472569341e6
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-03-28 14:46:19 -0300

    Criando controlador para cadastrar um cliente.

---
+++ src/main/java/br/com/zup/edu/multasmanager/controller/CadastrarNovoClienteController.java
@@ -0,0 +1,40 @@
+package br.com.zup.edu.multasmanager.controller;
+
+import br.com.zup.edu.multasmanager.model.Cliente;
+import br.com.zup.edu.multasmanager.repository.ClienteRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/clientes")
+public class CadastrarNovoClienteController {
+
+    private final ClienteRepository repository;
+
+    public CadastrarNovoClienteController(ClienteRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> cadastrar (@RequestBody @Valid ClienteRequest request, UriComponentsBuilder uriComponentsBuilder) {
+
+        Cliente novoCliente = request.paraCliente();
+
+        repository.save(novoCliente);
+
+        URI location = uriComponentsBuilder
+                .path("/clientes/{id}")
+                .buildAndExpand(novoCliente.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+
+    }
+}
